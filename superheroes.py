6a7a62fc629557a5d9930f0c019dd2000131a018
jaebooker <jaesonbooker@gmail.com>
2018-09-16 00:02:40 -0700

    added variables and class

---
+++ superheroes.py
@@ -0,0 +1,20 @@
+import random
+def attack(self):
+    # Return attack value
+
+def update_attack(self, attack_strength):
+    # Update attack value
+def add_ability(self, ability):
+    # Add ability to abilities list
+
+def attack(self):
+    # Run attack() on every ability hero has
+class Ability:
+    def __init__(self, name, attack_strength):
+        # Initialize starting values
+
+    def attack(self):
+        # Return attack value
+
+    def update_attack(self, attack_strength):
+        # Update attack value
